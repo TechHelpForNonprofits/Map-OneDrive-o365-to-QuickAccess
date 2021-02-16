07bdd5ff0285973a08191501fff2c9e7cda49b47
TechHelpForNonprofits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-02-16 11:14:14 -0600

    Update QuickAccess.ps1

--- QuickAccess.ps1
+++ QuickAccess.ps1
@@ -1,1 +1,5 @@
+$CompanyName = "disabilityrightstexas" ## full company name from Office 365 ex. "Microsoft"
+$SharepointLibrary = "Storage/test" ## site and library name ex. Storage/Test
+$DomainName = "_disabilityrightstx_org" ## full domain name including underscores ex. "_Microsoft_com"
+
 

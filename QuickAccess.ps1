a7f199437021f74eee8c11d82bcb0b988a86be73
TechHelpForNonProfits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-02-17 10:54:02 -0600

    Update QuickAccess.ps1

--- QuickAccess.ps1
+++ QuickAccess.ps1
@@ -6,14 +6,18 @@ $SharepointLibrary =  ## site name and library name ex. "Storage/Test"
 $DomainName =  ## full domain name including underscores ex. "_Microsoft_com"
 ## Do not change anything past this point
 
- $ie = Start-Process -file iexplore -arg 'https:// + $CompanyName + .sharepoint.com/sites/ + $SharepointLibrary' -PassThru -WindowStyle Minimized
+$urlSPLibrary = "https://" + $CompanyName + ".sharepoint.com\sites\" + $SharepointLibrary
+$urlOneDrive = "https://" + $CompanyName + "-my.sharepoint.com\personal\" + ${env:username} + $DomainName + "\Documents"
+
+
+ $ie = Start-Process -file iexplore -arg $urlSPLirbrary -PassThru -WindowStyle Minimized
  sleep 8
  $ie.Kill()
- $ie = Start-Process -file iexplore -arg "https:// + $CompanyName + -my.sharepoint.com/personal/${env:username}$DomainName\Documents" -Passthru -WindowStyle Minimized
+ $ie = Start-Process -file iexplore -arg $urlOneDrive -Passthru -WindowStyle Minimized
  sleep 8
  $ie.Kill()
- $folder = '\\$CompanyName.sharepoint.com@SSL\DavWWWRoot\sites\$SharepointLibrary'
- $folder1 = '\\$CompanyName + -my.sharepoint.com@SSL\DavWWWRoot\personal\' + $env:username + $DomainName + '\Documents'
+ $folder = "\\" + $CompanyName + ".sharepoint.com@SSL\DavWWWRoot\sites\" + $SharepointLibrary
+ $folder1 = "\\" + $CompanyName + "-my.sharepoint.com@SSL\DavWWWRoot\personal\" + $env:username + $DomainName + "\Documents"
  $QuickAccess = New-Object -ComObject shell.application
  $QuickAccess.Namespace($folder).Self.InvokeVerb("pintohome")
  $QuickAccess.Namespace($folder1).Self.InvokeVerb("pintohome")

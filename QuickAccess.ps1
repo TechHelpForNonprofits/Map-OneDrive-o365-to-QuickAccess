31611174bbeaf8d588be88f1459401f41f228836
TechHelpForNonProfits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-02-17 11:19:37 -0600

    Update QuickAccess.ps1

--- QuickAccess.ps1
+++ QuickAccess.ps1
@@ -1,16 +1,16 @@
 ## Written by Carlton Whitmore, 2021
 
 ## Please set these variables
-$CompanyName =  ## full company name from Office 365 ex. "Microsoft"
-$SharepointLibrary =  ## site name and library name ex. "Storage/Test"
-$DomainName =  ## full domain name including underscores ex. "_Microsoft_com"
+$CompanyName =   ## full company name from Office 365 ex. "Microsoft"
+$SharepointLibrary =   ## site name and library name ex. "Storage/Test"
+$DomainName =   ## full domain name including underscores ex. "_Microsoft_com"
 ## Do not change anything past this point
 
 $urlSPLibrary = "https://" + $CompanyName + ".sharepoint.com\sites\" + $SharepointLibrary
 $urlOneDrive = "https://" + $CompanyName + "-my.sharepoint.com\personal\" + ${env:username} + $DomainName + "\Documents"
 
 
- $ie = Start-Process -file iexplore -arg $urlSPLirbrary -PassThru -WindowStyle Minimized
+ $ie = Start-Process -file iexplore -arg $urlSPLibrary -PassThru -WindowStyle Minimized
  sleep 8
  $ie.Kill()
  $ie = Start-Process -file iexplore -arg $urlOneDrive -Passthru -WindowStyle Minimized
